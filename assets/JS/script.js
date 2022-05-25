a3ec682cb3a7b41bca1544d33884aa70208329ed
Kija <kmboard1@yahoo.com>
2022-05-25 00:00:44 -0400

    trying to get api to work

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,25 +1,33 @@
-var city= "";
+var apiKey = '61a0b954659200b06ebed8d8c24ace13';
+
+var city= [];
 var dateEl = document.getElementById ('date');
-var currentweather  = document.getElementById ('currentweather');
+var currentcity = document.getElementById ('currentcity');
 var searchBtn = document.getElementById ('searchBtn');
 var citySearch = document.getElementById ('citysearch');
-var icon = document.querySelector (".icon")
+var currenthumidity = document.getElementById('#humidity')
+var currenttemp = document.getElementById('#temp');
+var currentUV = document.getElementById('#UV')
+var currentwind =document.getElementById ('#wind');
+var icon = document.querySelector (".icon");
 
 var currentDate = moment().format("MM/DD/YYY");
 var recentSearch = [];
 
 
-var apiKey = '61a0b954659200b06ebed8d8c24ace13';
+
 
 function displayWeather(event) {
   event.preventDefault();
 if(citySearch.val().trim()!==""){
   city=citySearchval().trim();
   currentweather(city);
+}
 };
 
-function currentweather(lat,lon) {
-  var requestUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q" + city + "APPID=" + apiKey;
+function currentweather(city) {
+
+  var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.7489924&lon=-84.3902644&limit=1&appid=61a0b954659200b06ebed8d8c24ace13";
   fetch(requestUrl)
   .then(function (response) {
     if(response.ok) {
@@ -35,13 +43,18 @@ function currentweather(lat,lon) {
       console.log(data);
       getWeather(data);
       $(".weathercard").show();
-      renderFiveDay(data);
+      // renderForecast(data);
       $("#fivedatyforcastheader").show();
   }
 })
 }
 
-  function getWeather(weatherJSON) {
+function getWeather(weatherJSON) {
+  var queryURL= "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + city + "&appid=" + apiKey;
+  $.ajax({
+      url:queryURL,
+      method:'GET',
+  }).then(function(showWeather) {
     
     $("#curentweather").text($('#citylist').val() + "" + moment().format("(MM/DD/YY)"));
     $("#currentweather").append(
@@ -49,42 +62,73 @@ function currentweather(lat,lon) {
     )
       let iconurl ="http://openweathermap.org/img/w/" + weatherJSON.current.weather[0].icon + ".png";
     $('currentwicon').attr('src', iconurl);
-    $('currenttemp').text(weatherJSON.current.temp + tempUnit);
-    $('currentwind').text(weatherJSON.current.wind_speed + windSpeed);
-    $('currenthumidity').text(weatherJSON.current.humidity + "%");
+    $('currenttemp').text(weatherJSON.current.currenttemp);
+    $('currentwind').text(weatherJSON.current.currentwind);
+    $('currenthumidity').text(weatherJSON.current.currenthumidity);
     $('currentUV').text(weatherJSON.current.UV);
     
-  }
+  });
+}
+  // function renderForecast(data) {
+  //   $("#fivedayforecast").empty();
+  //   for (let i = 1; i <= 5; i++) {
+  //       let iconurl = "http://openweathermap.org/img/w/" + data.daily[i].weather[0].icon + ".png";
+  //       $("#fivedayforecast").append(
+  //           `<div class="dailycard col-2.4">
+  //           <div class="card-header">
+  //           ${moment().add(i, 'days').format("MM/DD/YY")}
+  //           <span id="icon"><img id="wicon${i}" src="${iconurl}" alt=""></span>
+  //           </div>
+  //           <p>Temp: <span>${parseInt(data.daily[i].temp.min)} - ${parseInt(data.daily[i].temp.max)}${tempUnit}</span></p>
+  //           <p>Wind: <span>${data.daily[i].wind_speed} ${windUnit}</span></p>
+  //           <p>Humidity: <span>${data.daily[i].humidity}%</span></p>
+  //           <p>UV Index: <span class="${checkUVIClass(data.daily[i].uvi)}">${data.daily[i].uvi}</span></p>
+  //         </div>`
+  //       )
+  //   }
+// }
 
 searchBtn.addEventListener("click", function (event) {
   event.preventDefault();
-  currentweather(lat,lon);
+  currentweather(city);
   let search = {
     city: $('#citylist').val(),
-    latitude: lat,
-    longitude: lon,
-  };
-
-  citySearch(search);
-})
-
-fetch (
-  'https://api.openweathermap.org/data/2.5/onecall?lat=33.7489924&lon=-84.3902644&limit=1&appid=61a0b954659200b06ebed8d8c24ace13'
-)
-.then(function (response) {
-  return response.json();
-})
-.then(function (data) {
-  console.log(data)
+  }
+    return search;
+  
 });
 
-$('#seachBtn').on("click", getWeather);
+
+
+
+function loadRecent() {
+  var recentSearch = JSON.parse(localStorage.getItem("reacentSearch")) || [];
+  if (recentSearch === null) {
+    return;
+  }
+  else {
+    cityHistory = recentSearch;
+    for (var i = 0; i < cityHistory.length; i++) {
+      var historyBtn = document.createElement("button");
+      historyBtn.textContent = cityHistory[i];
+      historyBtn.classList.add("history-btn");
+      historyBtn.setAttribute("id", "city-history");
+      searchContainer.appendChild(historyBtn);
+    }
+  }
+}
+
+var history = function(event) {
+  var pastHistory = event.target.textContent;
+  currentweather(pastHistory);
+  history();
+}
+
+
+
+
+$('#seachBtn').on("click", displayWeather);
   
 
 
-};
-
-
-
-    
 
